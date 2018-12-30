f960988677e440d573ecc318d1359689d20e2b7b
Victor Reyes Medina <victor.reyes.medina@gmail.com>
2018-12-30 20:02:03 -0300

    add some functions

--- Arduino-Mavlink/Arduino-Mavlink.ino
+++ Arduino-Mavlink/Arduino-Mavlink.ino
@@ -1,83 +1,133 @@
-/*  You can restrict the maximum package size with this parameter in mavlink_types.h:
-
-    #ifndef MAVLINK_MAX_PAYLOAD_LEN_
-    // it is possible to override this, but be careful! Defa_
-    #define **MAVLINK_MAX_PAYLOAD_LEN 255 ///< Maximum payload length_
-    #endif_
- */
 
 #include "mavlink.h"
-//#include "common/mavlink_msg_request_data_stream.h"
 
-// Mavlink variables
-unsigned long previousMillisMAVLink = 0;     // will store last time MAVLink was transmitted and listened
-unsigned long next_interval_MAVLink = 1000;  // next interval to count
-const int num_hbs = 60;                      // # of heartbeats to wait before activating STREAMS from Pixhawk. 60 = one minute.
+unsigned long previousMillisMAVLink = 0;    // will store last time MAVLink was transmitted and listened
+unsigned long next_interval_MAVLink = 1000; // next interval to count
+const int num_hbs = 60;                     // # of heartbeats to wait before activating STREAMS from Pixhawk. 60 = one minute.
 int num_hbs_pasados = num_hbs;
 
-// Lights flashing adjustment
-/*unsigned long previousMillis = 0;     // will store last time LED was updated
-unsigned long next_interval = 0;      // next interval
-const long tiempo_on = 20+;
-const long tiempo_off = 80;
-const long tiempo_descanso = 880;
-int test_led_tipo = 4;*/
 
-void setup() {
+// MAVLINK Parameters (header)
+int sysid = 1;                 ///< ID 20 for this airplane. 1 PX, 255 ground station
+int compid = 158;              ///< The component sending the message
+int type = MAV_TYPE_QUADROTOR; ///< This system is an airplane / fixed wing
+
+// Define the system type, in this case an airplane -> on-board controller
+// uint8_t system_type = MAV_TYPE_FIXED_WING;
+uint8_t system_type = MAV_TYPE_GENERIC;
+uint8_t autopilot_type = MAV_AUTOPILOT_INVALID;
+
+uint8_t system_mode = MAV_MODE_PREFLIGHT; ///< Booting up
+uint32_t custom_mode = 0;                 ///< Custom mode, can be defined by user/adopter
+uint8_t system_state = MAV_STATE_STANDBY; ///< System ready for flight
+
+void setup()
+{
   // MAVLink interface start
   Serial.begin(57600);
 }
 
-void loop() {
-
-  unsigned long currentMillis = millis();
-        
-  // MAVLink
-  /* The default UART header for your MCU */ 
-  int sysid = 1;                   ///< ID 20 for this airplane. 1 PX, 255 ground station
-  int compid = 158;                ///< The component sending the message
-  int type = MAV_TYPE_QUADROTOR;   ///< This system is an airplane / fixed wing
- 
-  // Define the system type, in this case an airplane -> on-board controller
-  // uint8_t system_type = MAV_TYPE_FIXED_WING;
-  uint8_t system_type = MAV_TYPE_GENERIC;
-  uint8_t autopilot_type = MAV_AUTOPILOT_INVALID;
- 
-  uint8_t system_mode = MAV_MODE_PREFLIGHT; ///< Booting up
-  uint32_t custom_mode = 0;                 ///< Custom mode, can be defined by user/adopter
-  uint8_t system_state = MAV_STATE_STANDBY; ///< System ready for flight
-  
-  // Initialize the required buffers
+void sendHeartBeat()
+{
   mavlink_message_t msg;
   uint8_t buf[MAVLINK_MAX_PACKET_LEN];
- 
-  // Pack the message
-  //mavlink_msg_heartbeat_pack(sysid,compid, &msg, type, autopilot_type, system_mode, custom_mode, system_state);
-  mavlink_msg_heartbeat_pack(1,0, &msg, type, autopilot_type, system_mode, custom_mode, system_state);
- 
-  // Copy the message to the send buffer
+
+  mavlink_msg_heartbeat_pack(sysid, compid, &msg, type, autopilot_type, system_mode, custom_mode, system_state);
   uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
- 
-  // Send the message with the standard UART send function
-  // uart0_send might be named differently depending on
-  // the individual microcontroller / library in use.
-  unsigned long currentMillisMAVLink = millis();
+  Serial.write(buf, len);
+}
+
+void takeOff()
+{
+  mavlink_message_t msg;
+  uint8_t buf[MAVLINK_MAX_PACKET_LEN];
   
-  if (currentMillisMAVLink - previousMillisMAVLink >= next_interval_MAVLink) {
-    
+  mavlink_msg_command_long_pack(sysid, compid, &msg, type, autopilot_type, MAV_CMD_NAV_TAKEOFF, 1, 0, 0, 0, 0, 0, 0, 1.0);
+  uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+  Serial.write(buf, len);
+}
+
+void land()
+{
+  mavlink_message_t msg;
+  uint8_t buf[MAVLINK_MAX_PACKET_LEN];
+
+  mavlink_msg_command_long_pack(sysid, compid, &msg, type, autopilot_type, MAV_CMD_NAV_LAND, 1, 0, 0, 0, 0, 0, 0, 0);
+  uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+  Serial.write(buf, len);
+}
+
+/**
+ * @param state true for arm, false for disarm 
+ */
+void changeArmingState(boolean state)
+{
+  mavlink_message_t msg;
+  uint8_t buf[MAVLINK_MAX_PACKET_LEN];
+
+  //Arm the drone using MAV_CMD_COMPONENT_ARM_DISARM command
+  // 1 an 8'th argument is for ARM (0 for DISARM)
+  if (state)
+  {
+    // ARM
+    mavlink_msg_command_long_pack(sysid, compid, &msg, type, autopilot_type, MAV_CMD_COMPONENT_ARM_DISARM, 1, 1.0, 0, 0, 0, 0, 0, 0 );
+  }
+  else
+  {
+    //DISARM
+    // param2 to 21196 will disarm on flight
+    mavlink_msg_command_long_pack(sysid, compid, &msg, type, autopilot_type, MAV_CMD_COMPONENT_ARM_DISARM, 1, 0, 0, 0, 0, 0, 0, 0 );
+  }
+
+  uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+  Serial.write(buf, len);
+}
+
+void loop()
+{
+
+  unsigned long currentMillis = millis();
+
+  sendHeartBeat();
+
+  unsigned long currentMillisMAVLink = millis();
+
+  if (currentMillisMAVLink - previousMillisMAVLink >= next_interval_MAVLink)
+  {
+
     // Guardamos la última vez que se cambió el modo
     previousMillisMAVLink = currentMillisMAVLink;
 
-    //Serial.write(buf, len);
 
-    //Mav_Request_Data();
     num_hbs_pasados++;
-    if(num_hbs_pasados>=num_hbs) {
+    if (num_hbs_pasados >= 60) // after one minute
+    {
       //Request streams from Pixhawk
       Mav_Request_Data();
-      num_hbs_pasados=0;
+      
+      
+      //num_hbs_pasados = 0;
     }
-    
+
+    if (num_hbs_pasados == 60)
+    {
+      changeArmingState(true);
+    }
+
+    if (num_hbs_pasados == 90)
+    {
+      takeOff();
+    }
+
+    if (num_hbs_pasados == 120)
+    {
+      land();
+    }
+
+    if (num_hbs_pasados == 130)
+    {
+      changeArmingState(false);
+    }
   }
 
   // Check reception buffer
@@ -88,7 +138,6 @@ void Mav_Request_Data()
 {
   mavlink_message_t msg;
   uint8_t buf[MAVLINK_MAX_PACKET_LEN];
-
 
   // STREAMS that can be requested
   /*
@@ -111,137 +160,100 @@ void Mav_Request_Data()
    */
 
   // To be setup according to the needed information to be requested from the Pixhawk
-  const int  maxStreams = 2;
+  const int maxStreams = 2;
   const uint8_t MAVStreams[maxStreams] = {MAV_DATA_STREAM_EXTENDED_STATUS, MAV_DATA_STREAM_EXTRA1};
-  const uint16_t MAVRates[maxStreams] = {0x02,0x05};
+  const uint16_t MAVRates[maxStreams] = {0x02, 0x05};
 
-    
-  for (int i=0; i < maxStreams; i++) {
-    /*
-     * mavlink_msg_request_data_stream_pack(system_id, component_id, 
-     *    &msg, 
-     *    target_system, target_component, 
-     *    MAV_DATA_STREAM_POSITION, 10000000, 1);
-     *    
-     * mavlink_msg_request_data_stream_pack(uint8_t system_id, uint8_t component_id, 
-     *    mavlink_message_t* msg,
-     *    uint8_t target_system, uint8_t target_component, uint8_t req_stream_id, 
-     *    uint16_t req_message_rate, uint8_t start_stop)
-     * 
-     */
+  for (int i = 0; i < maxStreams; i++)
+  {
     mavlink_msg_request_data_stream_pack(2, 200, &msg, 1, 0, MAVStreams[i], MAVRates[i], 1);
     uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
     Serial.write(buf, len);
   }
-  
-  // Request: PARAM_REQUEST_LIST. Only for full log recording
-  /*
-   * Primitive: mavlink_msg_param_request_list_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
-                   uint8_t target_system, uint8_t target_component)
-   */
-/*
-  // Configure
-  uint8_t system_id=2;
-  uint8_t component_id=200;
-  // mavlink_message_t* msg;
-  uint8_t target_system=1;
-  uint8_t target_component=0;
 
-  // Pack
-  mavlink_msg_param_request_list_pack(system_id, component_id, &msg,
-    target_system, target_component);
-  uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
-
-  // Send
-#ifdef SOFT_SERIAL_DEBUGGING
-    pxSerial.write(buf,len);
-#else
-    Serial.write(buf, len);
-#endif
-*/
 }
 
+void comm_receive()
+{
 
-
-void comm_receive() {
- 
   mavlink_message_t msg;
   mavlink_status_t status;
- 
+
   // Echo for manual debugging
   // Serial.println("---Start---");
 
-  while(Serial.available()>0) {
+  while (Serial.available() > 0)
+  {
     uint8_t c = Serial.read();
 
     // Try to get a new message
-    if(mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status)) {
+    if (mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status))
+    {
 
       // Handle message
-      switch(msg.msgid) {
-        case MAVLINK_MSG_ID_HEARTBEAT:  // #0: Heartbeat
-          {
-            Serial.println("Got a heartbeat");
-          }
-          break;
+      switch (msg.msgid)
+      {
+      case MAVLINK_MSG_ID_HEARTBEAT: // #0: Heartbeat
+      {
+        Serial.println("Got a heartbeat");
+      }
+      break;
 
-        case MAVLINK_MSG_ID_SYS_STATUS:  // #1: SYS_STATUS
-          {
-            /* Message decoding: PRIMITIVE
+      case MAVLINK_MSG_ID_SYS_STATUS: // #1: SYS_STATUS
+      {
+        /* Message decoding: PRIMITIVE
              *    mavlink_msg_sys_status_decode(const mavlink_message_t* msg, mavlink_sys_status_t* sys_status)
              */
-            //mavlink_message_t* msg;
-            mavlink_sys_status_t sys_status;
-            mavlink_msg_sys_status_decode(&msg, &sys_status);
-            
-          }
-          break;
+        //mavlink_message_t* msg;
+        mavlink_sys_status_t sys_status;
+        mavlink_msg_sys_status_decode(&msg, &sys_status);
+      }
+      break;
 
-        case MAVLINK_MSG_ID_PARAM_VALUE:  // #22: PARAM_VALUE
-          {
-            /* Message decoding: PRIMITIVE
+      case MAVLINK_MSG_ID_PARAM_VALUE: // #22: PARAM_VALUE
+      {
+        /* Message decoding: PRIMITIVE
              *    mavlink_msg_param_value_decode(const mavlink_message_t* msg, mavlink_param_value_t* param_value)
              */
-            //mavlink_message_t* msg;
-            mavlink_param_value_t param_value;
-            mavlink_msg_param_value_decode(&msg, &param_value);
-          }
-          break;
+        //mavlink_message_t* msg;
+        mavlink_param_value_t param_value;
+        mavlink_msg_param_value_decode(&msg, &param_value);
+      }
+      break;
 
-        case MAVLINK_MSG_ID_RAW_IMU:  // #27: RAW_IMU
-          {
-            /* Message decoding: PRIMITIVE
+      case MAVLINK_MSG_ID_RAW_IMU: // #27: RAW_IMU
+      {
+        /* Message decoding: PRIMITIVE
              *    static inline void mavlink_msg_raw_imu_decode(const mavlink_message_t* msg, mavlink_raw_imu_t* raw_imu)
              */
-            mavlink_raw_imu_t raw_imu;
-            mavlink_msg_raw_imu_decode(&msg, &raw_imu);
-          }
-          break;
+        mavlink_raw_imu_t raw_imu;
+        mavlink_msg_raw_imu_decode(&msg, &raw_imu);
+      }
+      break;
 
-        case MAVLINK_MSG_ID_ATTITUDE:  // #30
-          {
-            /* Message decoding: PRIMITIVE
+      case MAVLINK_MSG_ID_ATTITUDE: // #30
+      {
+        /* Message decoding: PRIMITIVE
              *    mavlink_msg_attitude_decode(const mavlink_message_t* msg, mavlink_attitude_t* attitude)
              */
-            mavlink_attitude_t attitude;
-            mavlink_msg_attitude_decode(&msg, &attitude);
-            Serial.write("Roll: ");
-            Serial.println(attitude.roll);
-           
-            Serial.write("Yaw: ");
-            Serial.println(attitude.yaw);
+        mavlink_attitude_t attitude;
+        mavlink_msg_attitude_decode(&msg, &attitude);
+        Serial.write("Roll: ");
+        Serial.println(attitude.roll);
 
-            Serial.write("Pitch: ");
-            Serial.println(attitude.pitch);
+        Serial.write("Yaw: ");
+        Serial.println(attitude.yaw);
 
-            Serial.write("Altitude: ");
-            Serial.println(attitude.);
-            delay(1000);
-          }
-          break;
+        Serial.write("Pitch: ");
+        Serial.println(attitude.pitch);
+
         
-       default:
-          break;
+        delay(1000);
+      }
+      break;
+
+      default:
+        break;
       }
     }
   }
